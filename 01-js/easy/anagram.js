408958cb8e15ef1b81f2e1484198b9b48b633625
vedant-io <vedantd004@gmail.com>
2024-01-02 16:30:02 +0530

    Added Hello World comment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,7 +3,7 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
-
+//Hello World
 function isAnagram(str1, str2) {
   if (str1.length!== str2.length) {
     return false;
